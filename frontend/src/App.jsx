b28ae391fe3750e529511074e71ce05d5cb717af
Swati Prangya Das <swatiprangya2003@gmail.com>
2025-12-25 10:27:23 +0530

    Add Navbar and sidebar folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './components/Employee/Navbar/Navbar'
+import Sidebar from './components/Employee/Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Sidebar/>
     </div>
   )
 }
